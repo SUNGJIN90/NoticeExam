55d364e5f1e39c65dff0bd3cbff6ad77c1cdccbc
pklil0519 <pklil0519@naver.com>
2022-07-12 22:00:46 +0900

    Write Value onChange

--- src/components/WriteComponent/index.js
+++ src/components/WriteComponent/index.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { WriteContainer, WriteTitle, WriteContent, CompletionBtn, CancelBtn } from './styles';
 
 const WriteComponent = () => {
+  const [titleValue, setTitleValue] = useState('');
+  const [contentValue, setContentValue] = useState('');
+
+  const handleTitle = (event) => {
+    setTitleValue(event.target.value);
+  };
+  console.log('title : ', titleValue);
+
+  const handleContent = (event) => {
+    setContentValue(event.target.value);
+  };
+  console.log('content : ', contentValue);
+
   return (
     <WriteContainer>
-      <WriteTitle>제목을 입력하세요</WriteTitle>
-      <WriteContent>내용을 입력하세요</WriteContent>
+      <WriteTitle>
+        <label>
+          제목:
+          <input type="text" name="title" value={titleValue} onChange={handleTitle} />
+        </label>
+      </WriteTitle>
+      <WriteContent>
+        내용:
+        <textarea name="content" value={contentValue} onChange={handleContent} />
+      </WriteContent>
       <CompletionBtn onClick={() => window.open('/', '_blank')}>
         <p>완료</p>
       </CompletionBtn>
